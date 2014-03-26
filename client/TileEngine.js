173b9582b68aef42b66d83ce3eca14940bbcd4ea
Tyler Jay Monson <celinfas@ta-26.cs.byu.edu>
2014-03-26 12:23:56 -0600

    Testing out my first commit - added server and client folders, as well as the TileEngine.js file

---
+++ client/TileEngine.js
@@ -0,0 +1,47 @@
+function TileEngine(node) {
+	
+	this.node = $(node);
+	this.map = null;
+	
+	this.drawMap = function() {
+		this.clearMap();
+	};
+	
+	this.clearMap = function() {
+		
+	};
+	
+	this.drawPlayer = function() {
+		this.clearPlayer();
+	};
+	
+	this.clearPlayer = function() {
+	
+	};
+	
+	this.drawMapItems = function() {
+		this.clearMapItems();
+	};
+	
+	this.clearMapItems = function() {
+	
+	}
+	
+	this.canMoveUp = function(node) {
+	
+	};
+	
+	this.canMoveDown = function(node) {
+	
+	};
+	
+	this.canMoveLeft = function(node) {
+	
+	};
+	
+	this.canMoveRight = function(node) {
+	
+	};
+	
+
+}
