9c882cba766d546c1f27bce7589410ea5371c206
Kevin Johansen <mrskybear@gmail.com>
2014-04-04 03:08:22 -0600

    Using port 80 for server

--- server/server.py
+++ server/server.py
@@ -6,4 +6,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
