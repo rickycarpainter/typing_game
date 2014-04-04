937c6b710cc6da5cdc0b04c6f26b633777a05ba5
Kevin Johansen <mrskybear@gmail.com>
2014-04-04 02:00:56 -0600

    Losing hope.

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'static/index.html'
+    return url_for('static/index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
