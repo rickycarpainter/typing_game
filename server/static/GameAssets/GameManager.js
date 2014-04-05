2ca63514d0dccd85147df0bfeb3061ecf2d9e490
ritchiejc <rickyjc@studentbody.byu.edu>
2014-04-05 01:44:21 -0600

    transferred changes to server

--- server/static/GameAssets/GameManager.js
+++ server/static/GameAssets/GameManager.js
@@ -72,11 +72,14 @@ function GameManager() {
 	
 	this.updatePasswordPrompt = function() {
 
+		//put focus on text field
+
+		//if hover over first time button, highlight
 		
 	
 		if(false){// if enter clicked, check if password is valid
-
-		}
+			//send query to server to check password
+		}	
 
 		if(false){ //if password is confirmed valid or new game selected or 'X' clicked
 			this.groupManager.closePasswordPrompt();
@@ -86,9 +89,19 @@ function GameManager() {
 	};
 	
 	this.updatePlayMode = function() {
-	
-		//switch()
 
-		//this is gonna be the hard part
+		//map and object should be drawn
+		//draw character's initial position
+		//generate random letters/numbers
+		//draw buttons based off those letters/numbers
+		//bring up any captions needed
+
+		//start game loop
+			//wait for input
+			//check input for matching letter
+			//animate buttons down
+			//move character
+			//check surroundings/collisions
+			//draw new buttons, only if direction isnt a wall
 	};
 }
--- server/static/GameAssets/GroupManager.js
+++ server/static/GameAssets/GroupManager.js
@@ -2,31 +2,42 @@ function GroupManager() {
 
 	this.init = function(gameWidth, gameHeight) {
 		
-		var anim = new $.gQ.Animation({ imageURL: "images/titlescreen.png",
+		var title = new $.gQ.Animation({ imageURL: "images/titlescreen.png",
 					numberOfFrame: 1,
-					delta: 707,
+					delta: gameWidth,
 					offsetx: 0,
 					offsety: 0,
 					type: $.gQ.ANIMATION_HORIZONTAL | $.gQ.ANIMATION_ONCE});
+		var spacebar = new $.gQ.Animation({ imageURL: "images/spacebar.png",
+					numberOfFrame: 2,
+					delta: 46,
+					offsetx: 0,
+					offsety: 0,
+					rate: 600,
+					type: $.gQ.ANIMATION_VERTICAL});
 					
-		$.playground().addGroup("mapgroup", {width: gameWidth, height: gameHeight, posx: 0, posy: 0})
-			//.scale(2)
-			.addGroup("mapBackground", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()
-			.addGroup("mapObjects", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()
-			.addGroup("mapForeground", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()	
+		$.playground()
 			.addGroup("titlescreen", {width: gameWidth, height: gameHeight, posx: 0, posy: 0})
-				.addSprite( "title", {animation: anim,
+				.addSprite( "title", {animation: title,
 										width: gameWidth,
 										height: gameHeight,
 										posx: 0,
 										posy: 0 
-										});
-			//ADD OTHER GROUPS HERE EX: HUD GROUP, OTHER SCREENS
-			
-			
-
-					
-				
+										})
+				.addSprite( "spacebar", {animation: spacebar,
+										width: 227,
+										height: 51,
+										posx: 240,
+										posy: 350 
+										})
+			.addGroup("selectionScreen", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()
+			.addGroup("levelSelection", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()
+			.addGroup("levelSelection", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()
+			.addGroup("mapgroup", {width: gameWidth, height: gameHeight, posx: 0, posy: 0})
+			.addGroup("mapBackground", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()
+			.addGroup("mapObjects", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()
+			.addGroup("mapForeground", {width: gameWidth, height: gameHeight, posx: 0, posy: 0}).end()
+			//ADD OTHER GROUPS HERE EX: HUD GROUP, OTHER SCREENS			
 
 	};
 	
@@ -35,22 +46,38 @@ function GroupManager() {
 	};
 
 	this.titleToSelection = function(){
-		$("#titlescreen").hide();
+		$("#titlescreen").fadeOut("medium");
+		//draw mode selection assets
+			//background
+			//mode buttons/images
 	};
 
 	this.openPasswordPrompt = function(){
+		//draw password prompt screen
+			//prompt
+			//'x' close
+			//text field
+			//first time button
 
 	};
 
 	this.closePasswordPrompt = function(){
+		//close all prompt stuff
 
 	};
 
 	this.selectionToLevels = function(){
-
+		//close mose selectio 
+		//check password that was entered
+		//draw all level selection assets
+			//backgound
+			//level images up to level of password entered
 	};
 
 	this.levelsToGame = function(){
-
+		//close level selection
+		//draw game
+			//background/tiles
+			//obects 
 	};
 }
