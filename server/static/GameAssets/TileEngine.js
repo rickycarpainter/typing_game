2cc5c6e4160e152f7306637b7ca09f482f89b1a3
MrSkyBear <mrskybear@gmail.com>
2014-04-12 15:03:16 -0600

    Syncing TileEngine for server

--- server/static/GameAssets/TileEngine.js
+++ server/static/GameAssets/TileEngine.js
@@ -1,10 +1,29 @@
 function TileEngine() {
 	
 	this.map = null;
+	this.player = null;
+	this.mapItems = null;
 	
+	//Clears and draws a new map if the map exists
+	//Only call this function once per map
 	this.drawMap = function() {
-		this.clearMap();
+		
+		if(this.map != null) {
+			this.map.clear();
+			this.map.draw();
+		}
 	};
+
+	this.downloadMap = function(mapID) {
+		
+		$.ajax({
+			url: '/DownloadMap',
+			data: mapID,
+			success: function (result) {
+				console.log(result);
+			}
+		});
+	}
 	
 	this.drawPlayer = function() {
 		this.clearPlayer();
@@ -22,28 +41,36 @@ function TileEngine() {
 	
 	}
 	
-	this.canMoveUp = function(node) {
+	this.canMoveUp = function(mapItem) {
+		return (mapItem!= null && //player exists
+					this.map != null && //map exists
+					mapItem.posX >= 0 && mapItem.posX < this.map.width && // player posX in map
+					mapItem.posY + 1 >= 0 && mapItem.posY + 1 < this.map.height && // player posY + 1 in map
+					this.map.mask[mapItem.posX][mapItem.posY + 1] != 2); // 2 is the collidable gID for the mask 
 	};
 	
-	this.canMoveDown = function(node) {
-	
+	this.canMoveDown = function() {
+		return (mapItem != null && //player exists
+					this.map != null && //map exists
+					mapItem.posX >= 0 && mapItem.posX < this.map.width && // player posX in map
+					mapItem.posY - 1 >= 0 && mapItem.posY - 1 < this.map.height && // player posY + 1 in map
+					this.map.mask[player.posX][player.posY - 1] != 2); // 2 is the collidable gID for the mask 	
 	};
 	
-	this.canMoveLeft = function(node) {
-	
+	this.canMoveLeft = function() {
+		return (mapItem != null && //player exists
+					this.map != null && //map exists
+					mapItem.posX - 1>= 0 && mapItem.posX - 1< this.map.width && // player posX in map
+					mapItem.posY >= 0 && mapItem.posY < this.map.height && // player posY + 1 in map
+					this.map.mask[player.posX - 1][player.posY] != 2); // 2 is the collidable gID for the mask 
 	};
 	
-	this.canMoveRight = function(node) {
-	
+	this.canMoveRight = function() {
+		return (mapItem != null && //player exists
+					this.map != null && //map exists
+					mapItem.posX + 1 >= 0 && mapItem.posX + 1< this.map.width && // player posX in map
+					mapItem.posY >= 0 && mapItem.posY < this.map.height && // player posY + 1 in map
+					this.map.mask[player.posX + 1][player.posY] != 2); // 2 is the collidable gID for the mask 
 	};
-	
-	this.setMovePrompts = function() {
-	
-	};
-	
-	this.clearMovePrompts = function() {
-	
-	};
-	
 
 }
