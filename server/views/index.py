b804ebbfaa726385195ac452239784b32075581f
MrSkyBear <mrskybear@gmail.com>
2014-04-12 17:53:06 -0600

    More trouble than it's worth

--- server/views/index.py
+++ server/views/index.py
@@ -24,15 +24,13 @@ def show():
 def game():
     login = False
     name = None
-    level = None
     if current_user.is_authenticated():
         login = True 
         if current_user.name:
             name = current_user.name
-            level = current_user.highest_level
         else:
             name = current_user.username
-    return render_template('Game.html', login=login, name=name, level_selected=level)
+    return render_template('Game.html', login=login, name=name)
 
 @index.route('/Game/DownloadMap', methods=['POST'])
 def downloadMap():
