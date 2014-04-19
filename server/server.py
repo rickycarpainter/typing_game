0291a95ea78efaaf841f6bc7e9cfb33a55b7e317
MrSkyBear <mrskybear@gmail.com>
2014-04-19 12:39:44 -0600

    More Server changes

--- server/server.py
+++ server/server.py
@@ -11,4 +11,4 @@ app.register_blueprint(auth, url_prefix="/auth")
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
