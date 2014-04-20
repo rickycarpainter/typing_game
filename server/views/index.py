e7c27f4bdaa2894b4f510475fe3180216bb02ede
MrSkyBear <mrskybear@gmail.com>
2014-04-19 19:26:19 -0600

    Added route to increment user level

--- server/views/index.py
+++ server/views/index.py
@@ -34,29 +34,35 @@ def game():
 
 @index.route('/Game/DownloadMap', methods=['POST'])
 def downloadMap():
-	mapID = int(request.form['id'])
-	dac = database_access()
-	result = dac.get_map_with_items(mapID)
-	if (result):
-            return result
-        else:
-            return jsonify(result="Map Not Found")
+    mapID = int(request.form['id'])
+    dac = database_access()
+    result = dac.get_map_with_items(mapID)
+    if (result):
+        return result
+    else:
+        return jsonify(result="Map Not Found")
 
 @index.route('/Game/HighestUserLevel', methods=['GET'])
 def highestLevel():
-        if current_user.is_authenticated():
-            print ("Requesting Level")
-            highest_level = current_user.level
-            print ("Highest Level: [" + str(highest_level) + "]")
-            return jsonify(result=highest_level)
-        print ("User not authenticated. Returning 1")
-        return jsonify(result=1)
+    if current_user.is_authenticated():
+        print ("Requesting Level")
+        highest_level = current_user.level
+        print ("Highest Level: [" + str(highest_level) + "]")
+        return jsonify(result=highest_level)
+    print ("User not authenticated. Returning 1")
+    return jsonify(result=1)
         
 @index.route('/Game/AllLevels', methods=['GET'])
 def alllevels():
-	dac = database_access()
-	response = dac.get_number_of_levels()
-	print ("All  levels: [" + str(response) + "]") 
-	return jsonify(result=response)
+    dac = database_access()
+    response = dac.get_number_of_levels()
+    print ("All  levels: [" + str(response) + "]") 
+    return jsonify(result=response)
+
+@index.route('/Game/NewLevelUnlocked')
+def incrementLevel():
+    if current_user.is_authenticated():
+        current_user.level++
+    
 		
 	
