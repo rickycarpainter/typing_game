e81830c765fac83c8efbe358a66836c7a83882d8
Kevin Johansen <mrskybear@gmail.com>
2014-04-20 18:28:40 -0600

    Clean up.

--- server/config.py
+++ server/config.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-SECRET_KEY = '\xda\x0f\x13\xa6\x08\xec\x87/_\r:\xbc\xd5\x0e\xb3\x92\xddI|\x18\xc7<\xd8"'
+SECRET_KEY = ''
 
-TWITTER_KEY = 'MDi4RAMnDpIfdUZeJuCfblUbt'
-TWITTER_SECRET = 'jz908y9V4KP7MBAsMasClGj71bqxkBMRfaeTcpL1SB9QocugUD'
+TWITTER_KEY = ''
+TWITTER_SECRET = ''
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
