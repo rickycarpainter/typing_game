ce494faaad1f5256b7160043afdade3cd291c3a8
MrSkyBear <mrskybear@gmail.com>
2014-04-22 00:32:36 -0600

    Tracking down error

--- server/static/GameAssets/StopWatch.js
+++ server/static/GameAssets/StopWatch.js
@@ -10,12 +10,7 @@ var StopWatch = function() {
 	this.display = function()
 	{
 		var current = Date(this.start_time);
-		var result = "";
-		result += current.getMinutes();
-		result += ": ";
-		result += current.getSeconds();
-		result += ": ";
-		result += current.getMilliseconds();
-		return result;
+		console.log(current.getSeconds());
+		return "00: 00: 00";
 	}
 };
